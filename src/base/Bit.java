cb7992102cae3609fd70f15f4ecc6952060a4087
loster12520 <xiaoshuo12520@outlook.com>
2023-09-29 23:00:38 +0800

    2023.09.29  22.59 补全了Bite的注释 增加了BiteUtils工具类

---
+++ src/base/Bit.java
@@ -0,0 +1,64 @@
+package base;
+
+/**
+ * 这是一个用于形容比特的类，请注意，本项目仅有这里还有对应这个类的构造函数会使用到除了函数引用之外的计算符号，也就是说，除了这里之外的所有的计算符号都会使用函数调用来代替
+ * 也就是说，我需要重构半个java库
+ * @author loster12520
+ */
+public class Bit {
+    /**
+     * 储存数值的位置
+     */
+    private boolean value;
+
+    /**
+     * 使用布尔变量创建bit值
+     * @param value  传入布尔值
+     */
+    public Bit(boolean value) {
+        this.value = value;
+    }
+
+    /**
+     * 使用int值创建bit值
+     * @param value  传入数字
+     */
+    public Bit(int value) {
+        this.value = value==0 ? false : true;
+    }
+
+    /**
+     * 判断是否相等
+     * @param b  需要与其对比的bit值
+     * @return  返回是否相等
+     */
+    public Bit equals(Bit b){
+        return new Bit(b.value==value);
+    }
+
+    /**
+     * 取反
+     * @return  返回取反之后的值
+     */
+    public Bit negation(){
+        return new Bit(!value);
+    }
+
+    /**
+     * 与
+     * @param bit  操作值
+     * @return  返回结果
+     */
+    public Bit and(Bit bit){
+        return new Bit(value?bit.value:false);
+    }
+
+    /**
+     * 或
+     * @param bit  操作值
+     * @return  返回结果
+     */
+    public Bit or(Bit bit){
+        return new Bit(value?true: bit.value);
+    }
+}
---
+++ src/base/BiteUtils.java
@@ -0,0 +1,54 @@
+package base;
+
+/**
+ * 关于Bit.java的工具类，以后看到类后缀是-Utils就是对应类的工具类，里面的所有方法都应该是static的
+ */
+public class BiteUtils {
+    /**
+     * True的值
+     */
+    public static final Bit True=new Bit(true);
+    /**
+     * False的值
+     */
+    public static final Bit False=new Bit(false);
+
+    /**
+     * 判断是否相等，是Bit中的equals方法的替代品，只不过是不同写法罢了
+     * @param bit1  操作值1
+     * @param bit2  操作值2
+     * @return  返回结果
+     */
+    public static Bit equals(Bit bit1,Bit bit2){
+        return bit1.equals(bit2);
+    }
+
+    /**
+     * 取与，是Bit中的and方法的替代品，只不过是不同写法罢了
+     * @param bit1  操作值1
+     * @param bit2  操作值2
+     * @return  返回结果
+     */
+    public static Bit and(Bit bit1,Bit bit2){
+        return bit1.and(bit2);
+    }
+
+    /**
+     * 取或，是Bit中的or方法的替代品，只不过是不同写法罢了
+     * @param bit1  操作值1
+     * @param bit2  操作值2
+     * @return  返回结果
+     */
+    public static Bit or(Bit bit1,Bit bit2){
+        return bit1.or(bit2);
+    }
+
+    /**
+     * 取反，是Bit中的negation方法的替代品，只不过是不同写法罢了
+     * @param bit  操作值
+     * @return  返回结果
+     */
+    public static Bit negation(Bit bit){
+        return bit.negation();
+    }
+}
